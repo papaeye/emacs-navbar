b654cc3382533fa3cc29be19c116908e63d0973c
papaeye <papaeye@gmail.com>
2015-01-11 02:44:50 +0900

    Fix library headers

--- navbarx-time.el
+++ navbarx-time.el
@@ -4,8 +4,6 @@
 
 ;; Author: papaeye <papaeye@gmail.com>
 ;; Keywords: convenience
-;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
