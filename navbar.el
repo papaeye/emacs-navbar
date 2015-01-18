142c91e76014905532b6cda6795d65b9601f7c55
papaeye <papaeye@gmail.com>
2015-01-19 04:44:18 +0900

    Fix doc

--- navbar.el
+++ navbar.el
@@ -25,7 +25,7 @@
 
 ;; navbar.el is a navigation bar for Emacs.
 ;; Just like the navbar component of Bootstrap <http://getbootstrap.com/>,
-;; navbar.el can contain several components called navbar items.
+;; navbar.el can contain various components called navbar items.
 ;;
 ;; navbar.el requires Emacs 24.3 or later.
 
